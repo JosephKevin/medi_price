326e86faf9fd621886c6270fa4fb5e14dabe0f82
Joseph Kevin Machado <jkm2155@columbia.edu>
2018-08-18 17:00:24 -0400

    get predicted price feature

--- dataOps.py
+++ dataOps.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, jsonify
 import json
+import pymysql.cursors
+
+
 class dataOps(object):
     def __init__(self):
         """
         Constructor for now it doesnt have any input parameters
         """
-        # ToDo: get credentials from a config file
-        self.credentials = {'host': '', 'username': '', 'password': '', 'port': ''}
-        pass
+        with open(r'./creds.json') as f:
+            self.credentials = json.load(f)
 
-    def get_prediction(self, procedure, state):
+    def get_prediction(self, procedure='sample', state='NY'):
         """
         Function to get the predicted values for the given procedure and state from the DB
 
@@ -18,7 +20,23 @@ class dataOps(object):
         :return: json object of the structure {'predicted_cost': 5000}
             the predicted_cost is always in USD for now
         """
-        return json.dumps({'price': 120})
+        # 1. connect to the DB
+        connection = pymysql.connect(host=self.credentials['host'],
+                                     user=self.credentials['username'],
+                                     password=self.credentials['password'],
+                                     db='medical_data',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        # 2. get the prediction for given procedure and state
+        get_pred_cost_sql = "select `Average Total Payments` from price_prediction where `DRG Definition` = {procedure} and' \
+                            '`Provider State` = {provider_state}".format(procedure=procedure, provider_state='state')
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(get_pred_cost_sql)
+                result = cursor.fetchone()
+        finally:
+            connection.close()
+        # 3. return the predicted_cost
+        return json.dumps({'predicted_cost': result['Average Total Payments']})
 
 
 """ Flask code for the API """
